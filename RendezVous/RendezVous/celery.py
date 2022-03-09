934b2d723d583d47168920c4ccfe563a3b3ef99b
jjjeanmax <jjjmaxime@gmail.com>
2022-03-09 13:30:09 +0300

    celery

--- RendezVous/RendezVous/celery.py
+++ RendezVous/RendezVous/celery.py
@@ -9,15 +9,6 @@ app = Celery('RendezVous')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-# app.conf.beat_schedule = {
-#     # Обновление данных при необходимости для каждого устройства
-#     'check_needly_update_data_from_devices': {
-#         'task': 'mqtt_client.tasks.check_needly_update_data_from_devices',
-#         # Каждую 10 секунд
-#         'schedule': 10,
-#     },
-# }
-
 
 @app.task(bind=True)
 def debug_task(self):
